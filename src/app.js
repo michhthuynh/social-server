32527909af4a278f90d6683f43d17edbddb733d3
harry hoang <huynhhoang.st.97@gmail.com>
2021-08-07 23:21:21 +0700

    setup prettier eslint

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-const PORT = process.env.PORT || 5000
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-const router = require('./routes')
-const logging = require('morgan')
-require('./utils/connectMongodb')()
+const PORT = process.env.PORT || 5000;
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const router = require('./routes');
+const logging = require('morgan');
+require('./utils/connectMongodb')();
 
-app.use(logging('tiny'))
-app.use(cors())
-app.use(express.json())
+app.use(logging('tiny'));
+app.use(cors());
+app.use(express.json());
 
-app.use('/api', router)
+app.use('/api', router);
 
-app.listen(PORT, () => console.log(`This server is running at ${PORT}`))
+app.listen(PORT, () => console.log(`This server is running at ${PORT}`));
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,87 +1,105 @@
-const UserModel = require("../models/User")
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const UserModel = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 module.exports.login = async (req, res) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
 
   const user = await UserModel.find({ email: email });
   if (!user[0]) {
     res.status(400).json({
-      message: 'Email does not register'
+      message: 'Email does not register',
     });
     return;
   }
 
-  const match = await bcrypt.compare(password, user[0]['password'])
+  const match = await bcrypt.compare(password, user[0]['password']);
 
   if (!match) {
     res.status(401).json({
-      message: 'Wrong password.'
-    })
+      message: 'Wrong password.',
+    });
     return;
   }
 
   // register token
-  console.log('Verify account successfully')
-  jwt.sign({ email }, process.env.SECRET_TOKEN, { expiresIn: '24h' }, (err, token) => {
-    if (err) {
-      console.log(err.message)
-      res.sendStatus(503)
-      return
-    }
-    res.status(200).json({
-      id: user[0].email,
-      token
-    })
-    return
-  })
-}
+  console.log('Verify account successfully');
+  jwt.sign(
+    { email },
+    process.env.SECRET_TOKEN,
+    { expiresIn: '24h' },
+    (err, token) => {
+      if (err) {
+        console.log(err.message);
+        res.sendStatus(503);
+        return;
+      }
+      res.status(200).json({
+        id: user[0].email,
+        token,
+      });
+      return;
+    },
+  );
+};
 
 module.exports.register = async (req, res) => {
-  const { firstName, lastName, email, password, gender, age, profilePicture, coverPicture, city, from, relationship } = req.body
+  const {
+    firstName,
+    lastName,
+    email,
+    password,
+    gender,
+    age,
+    profilePicture,
+    coverPicture,
+    city,
+    from,
+    relationship,
+  } = req.body;
+
   // check firstName and lastName is valid
-  const pattern = /^([^0-9]*)$/
+  const pattern = /^([^0-9]*)$/;
   if (!pattern.test(firstName)) {
     res.status(400).json({
-      msg: 'First name shouldn\'t have number'
-    })
-    return
+      msg: "First name shouldn't have number",
+    });
+    return;
   }
 
   if (!pattern.test(lastName)) {
     res.status(400).json({
-      msg: 'Last name shouldn\'t have number'
-    })
-    return
+      msg: "Last name shouldn't have number",
+    });
+    return;
   }
 
   // Check gender is enum
-  const genderEnum = [1, 2, 3]
+  const genderEnum = [1, 2, 3];
   if (!genderEnum.includes(gender)) {
     res.status(400).json({
-      msg: 'Gender invalid'
-    })
-    return
+      msg: 'Gender invalid',
+    });
+    return;
   }
 
   try {
-    const hasEmail = await UserModel.find({ email: email })
+    const hasEmail = await UserModel.find({ email: email });
     if (hasEmail.length !== 0) {
       res.status(400).json({
-        msg: 'Email is exist. Please use another email'
-      })
-      return
+        msg: 'Email is exist. Please use another email',
+      });
+      return;
     }
   } catch (error) {
-    console.log('Cannot connect to database:')
-    console.log(error)
-    res.sendStatus(503)
-    return
+    console.log('Cannot connect to database:');
+    console.log(error);
+    res.sendStatus(503);
+    return;
   }
 
   try {
-    const hashPassword = await bcrypt.hash(password, 10)
+    const hashPassword = await bcrypt.hash(password, 10);
     const user = await UserModel.create({
       firstName,
       lastName,
@@ -94,9 +112,9 @@ module.exports.register = async (req, res) => {
       city,
       from,
       relationship: relationship || 3,
-    })
+    });
     if (user) {
-      console.log(`Created database: ${user.id}`)
+      console.log(`Created database: ${user.id}`);
       res.status(201).json({
         firstName,
         lastName,
@@ -107,15 +125,15 @@ module.exports.register = async (req, res) => {
         coverPicture,
         city,
         from,
-      })
+      });
     } else {
-      console.log(`Cannot create account: ${email}`)
-      res.sendStatus(400)
-      return
+      console.log(`Cannot create account: ${email}`);
+      res.sendStatus(400);
+      return;
     }
   } catch (error) {
-    console.log(error)
-    res.sendStatus(503)
-    return
+    console.log(error);
+    res.sendStatus(503);
+    return;
   }
-}
+};
